dec3a9a313fe3c4c6f6a636f2fa8e2fab4d74ea3
soztalha <23050111069@aybu.edu.tr>
2025-08-29 02:02:12 +0300

    partners and mobileApp added

--- PremiumBank/PremiumBank/src/sections/MobileApp/MobileApp.jsx
+++ PremiumBank/PremiumBank/src/sections/MobileApp/MobileApp.jsx
@@ -1,9 +1,34 @@
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { BsApple } from "react-icons/bs";
+import { FaGooglePlay } from "react-icons/fa";
+import MobileAppMockup from "./MobileAppMockup/MobileAppMockup.jsx";
 import "./MobileApp.scss";
 
 function MobileApp() {
-    return(
-        <p>MobileApp</p>
-    );
+  return (
+    <section className="mobileapp-section">
+      <div className="mobileapp-content">
+        <div className="mobileapp-info">
+          <h2 className="mobileapp-title">Mobil Bankacılık Uygulaması</h2>
+          <p className="mobileapp-desc">
+            Kredi kartlarınızı yönetin, harcamalarınızı takip edin ve ödemelerinizi kolayca yapın. Her zaman yanınızda olan bankacılık deneyimi.
+          </p>
+          <ul className="mobileapp-features">
+            <li><IoMdCheckmarkCircleOutline  className="check-icon"/> Anında bildirimler</li>
+            <li><IoMdCheckmarkCircleOutline className="check-icon"/> Biyometrik güvenlik</li>
+            <li><IoMdCheckmarkCircleOutline className="check-icon"/> Harcama analizi</li>
+          </ul>
+          <div className="mobileapp-buttons">
+            <a href="#" className="appstore-btn"> <span role="img" aria-label="apple"><BsApple/></span> App Store </a>
+            <a href="#" className="googleplay-btn"> <span role="img" aria-label="google"><FaGooglePlay/></span> Google Play </a>
+          </div>
+        </div>
+        <div className="mobileapp-mockup">
+          <MobileAppMockup />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default MobileApp;
--- PremiumBank/PremiumBank/src/sections/Partners/Partners.jsx
+++ PremiumBank/PremiumBank/src/sections/Partners/Partners.jsx
@@ -1,9 +1,55 @@
 import "./Partners.scss";
+import PartnerCard from "./PartnerCard/PartnerCard.jsx";
+
+const partners = [
+  {
+    name: "VISA",
+    description: "Güvenli Ödeme",
+    logo: "/partners_logo/visa_logo.png", 
+  },
+  {
+    name: "Mastercard",
+    description: "Mastercard",
+    logo: "/partners_logo/mastercard_logo.svg", 
+  },
+  {
+    name: "AMEX",
+    description: "Premium",
+    logo: "/partners_logo/amex_logo.png", 
+  },
+    {
+    name: "Apple Pay",
+    description: "Mobil Ödeme",
+    logo: "/partners_logo/applepay_logo.png", 
+  },
+  {
+    name: "G Pay",
+    description: "Dijital Cüzdan",
+    logo: "/partners_logo/gpay_logo.webp",
+  },
+  {
+    name: "S Pay",
+    description: "Temassız",
+    logo: "/partners_logo/spay_logo.png",
+  },
+
+];
 
 function Partners() {
-    return(
-        <p>Partners</p>
-    );
+  return (
+    <section className="partners-section">
+      <h2 className="partners-title">Güvenilir Ortaklarımız</h2>
+      <p className="partners-desc">
+        Dünya çapında tanınmış markalarla işbirliği yaparak size en iyi hizmeti
+        sunuyoruz.
+      </p>
+      <div className="partners-list">
+        {partners.map((partner) => (
+          <PartnerCard key={partner.name} {...partner} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Partners;
