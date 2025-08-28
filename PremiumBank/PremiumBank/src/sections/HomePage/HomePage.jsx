e678f8429b90af207551a0e1e697b232ff64a4b8
Aysenurkislioglu <aysenurkislioglu@gmail.com>
2025-08-28 18:02:56 +0300

    Update HomePage

--- PremiumBank/PremiumBank/src/sections/HomePage/HomePage.jsx
+++ PremiumBank/PremiumBank/src/sections/HomePage/HomePage.jsx
@@ -1,38 +1,7 @@
-import React from "react";
 import "./HomePage.scss";
 
-const HomePage = () => {
-  return (
-    <section className="home">
-      <div className="home__cards">
-        {/* Card Stack */}
-        <div className="card-stack">
-          <div className="card platinum">PLATINUM</div>
-          <div className="card gold">GOLD</div>
-          <div className="card classic">CLASSIC</div>
-        </div>
-      </div>
-
-      <div className="home__content">
-        <h1>
-          Hayalinizdeki Kredi Kartı <br />
-          <span>Artık Elinizde</span>
-        </h1>
-        <p>
-          Yüksek limitler, düşük faizler ve benzersiz avantajlarla dolu kredi
-          kartlarımızla finansal özgürlüğünüzü keşfedin.
-        </p>
-        <div className="home__buttons">
-          <a href="#contact" className="btn btn--primary">
-            Hemen Başvur
-          </a>
-          <a href="#cards" className="btn btn--secondary">
-            Kartları İncele
-          </a>
-        </div>
-      </div>
-    </section>
-  );
-};
+function HomePage() {
+  return <p>HomePage</p>;
+}
 
 export default HomePage;
