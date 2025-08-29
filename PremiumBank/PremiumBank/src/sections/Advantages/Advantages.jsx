dc9a76d0f0f2f0e43839b3b4a52703a4eaf2bff7
Mustafa BULUT <mustbulut3@gmail.com>
2025-08-29 18:54:38 +0300

    blog ve advantages
    blog ve advantages kısımları eklendi

--- PremiumBank/PremiumBank/src/sections/Advantages/Advantages.jsx
+++ PremiumBank/PremiumBank/src/sections/Advantages/Advantages.jsx
@@ -1,9 +1,55 @@
 import "./Advantages.scss";
+import { FaCheckCircle } from "react-icons/fa";
+
 
 function Advantages() {
-    return(
-        <p>Advantages</p>
-    );
+  
+  const features = [
+    ` 7/24 Müşteri Hizmetleri`,
+    ` Anında Onay Sistemi`,
+    ` Güvenli Online İşlemler`,
+  ];
+
+  const stats = [
+    { value: "1M+", label: "Aktif Müşteri" },
+    { value: "99%", label: "Müşteri Memnuniyeti" },
+    { value: "25+", label: "Yıl Deneyim" },
+    { value: "24/7", label: "Hizmet" },
+  ];
+
+
+  return (
+
+    <div className="advantages">
+
+      <div className="advantages__left">
+        <h1>Neden PremiumBank?</h1>
+        <p>25 yılı aşkın bankacılık deneyimimiz ile 1 milyon+ müşteriye hizmet
+          veriyoruz. Güvenilir, yenilikçi ve müşteri odaklı yaklaşımımızla
+          finansal ihtiyaçlarınızda yanınızdayız.
+        </p>
+        <ul>
+          {features.map((x, i) => (
+
+            <li key={i}> <FaCheckCircle />  {x}</li>
+
+          ))}
+        </ul>
+      </div>
+
+      <div className="advantages__right">
+        {stats.map((x, i) => (
+          <div className="stat" key={i}>
+
+            <h3>{x.value}</h3>
+            <p>{x.label}</p>
+
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Advantages;
+
--- PremiumBank/PremiumBank/src/sections/Blog/Blog.jsx
+++ PremiumBank/PremiumBank/src/sections/Blog/Blog.jsx
@@ -1,9 +1,93 @@
 import "./Blog.scss";
+import { CiCreditCard2 } from "react-icons/ci";
+import { HiCurrencyDollar } from "react-icons/hi";
+import { LuSmartphoneNfc } from "react-icons/lu";
+import { FaArrowRight } from "react-icons/fa";
+
+const defaultPost = [
+  {
+    id: "kredi-karti-rehberi",
+    category: "Kredi Kartı Rehberi",
+    categoryKey: "guide",
+    icon: <CiCreditCard2 />,
+    title: "Kredi Kartı Seçerken Dikkat Edilmesi Gerekenler",
+    excerpt:
+      "Doğru kredi kartını seçmek için faiz oranları, yıllık ücretler ve avantajların karşılaştırılmasının önemi...",
+    date: "2024-12-15",
+    href: "#",
+  },
+  {
+    id: "borc-yonetimi",
+    category: "Finansal İpuçları",
+    categoryKey: "tips",
+    icon: <HiCurrencyDollar />,
+    title: "Kredi Kartı Borcunu Nasıl Yönetirsiniz?",
+    excerpt:
+      "Kredi kartı borcunuzu kontrol altında tutmak ve faiz yükünden kurtulmak için pratik stratejiler...",
+    date: "2024-12-12",
+    href: "#",
+  },
+  {
+    id: "temassiz-odeme-guvenligi",
+    category: "Teknoloji",
+    categoryKey: "tech",
+    icon: <LuSmartphoneNfc />,
+    title: "Temassız Ödeme Güvenliği",
+    excerpt:
+      "NFC teknolojisi ile yapılan temassız ödemelerin güvenlik özellikleri ve kullanım ipuçları...",
+    date: "2024-12-10",
+    href: "#",
+  }
+
+]
 
 function Blog() {
-    return(
-        <p>Blog</p>
-    );
+  return (
+
+
+    <div className="blog">
+
+      <div className="blog__header">
+        <h1>Finansal Rehber</h1>
+        <p>
+          Kredi kartları ve kişisel finans hakkında faydalı bilgiler ve güncel
+          haberler.
+        </p>
+
+      </div>
+
+      <div className="blog__content">
+        {defaultPost.map((x) => (
+
+          <div className="blog-card" key={x.id}>
+
+            <div className={`blog-card__top is-${x.categoryKey}`}>
+              {x.icon}
+              <p>{x.category}</p>
+            </div>
+
+            <div className="blog-card__bottom">
+
+              <h3>{x.title}</h3>
+              <p>{x.excerpt}</p>
+
+              <div className="blog-card__bottom__footer">
+                <p>{x.date}</p>
+                <a href={x.href} >Devamını Oku <FaArrowRight /></a>
+              </div>
+
+            </div>
+
+          </div>
+        ))}
+
+      </div>
+    </div>
+
+
+
+  );
 }
 
 export default Blog;
+
